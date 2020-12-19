d3f4a9567092785786d212d696097a2be84093cd
Samiullah Khan <samiullah.khan.1989@gmail.com>
2020-12-19 15:14:20 +0400

    basic tile texture and pixi viewport setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
-import { Application } from "pixi.js";
-
-const pixi_wrapper = document.querySelector('.pixi-wrapper') as HTMLElement;
+import { Application, Texture, TilingSprite } from "pixi.js";
+import { Viewport } from "pixi-viewport";
 
 const app = new Application({
-  resizeTo: pixi_wrapper,
   backgroundColor: 0x1d1d1d
 })
+
+const viewport = new Viewport({
+  interaction: app.renderer.plugins.interaction
+})
+
+viewport.drag().decelerate()
+
+app.stage.addChild(viewport)
+
+const texture = Texture.from('p2.jpeg')
+
+const tilingSprite = new TilingSprite(texture, app.screen.width, app.screen.height)
+
+app.stage.addChild(tilingSprite)
+
+// app.ticker.add(() => {
+//   tilingSprite.tilePosition.x += 1
+//   tilingSprite.tilePosition.y += 1
+// })
+
+document.body.appendChild(app.view)
