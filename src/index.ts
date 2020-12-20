be361ff3fa1168bd4d419e348a7c69804b2359db
Samiullah Khan <samiullah.khan.1989@gmail.com>
2020-12-20 11:42:10 +0400

    margin

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import { Application, Texture, TilingSprite } from "pixi.js";
 import { Viewport } from "pixi-viewport";
+import * as PixiJSGrid from "pixijs-grid"
+
+const  WORLDWIDTH = 2000
+const  WORLDHEIGHT = 2000
 
 const app = new Application({
-  backgroundColor: 0x1d1d1d
+  backgroundColor: 0xffffff,
+  width: WORLDWIDTH,
+  height: WORLDHEIGHT
 })
 
 const viewport = new Viewport({
@@ -10,14 +16,24 @@ const viewport = new Viewport({
 })
 
 viewport.drag().decelerate()
+console.log('viewport: ', viewport);
+viewport.position.set(innerWidth/2, innerHeight/2)
+viewport.on('moved', event => {
+  console.log('event: ', event.viewport.position);
+  console.log('moved');
+  tilingSprite.position.x = viewport.position.x
+  tilingSprite.position.y = viewport.position.y
+})
 
 app.stage.addChild(viewport)
 
 const texture = Texture.from('p2.jpeg')
 
-const tilingSprite = new TilingSprite(texture, app.screen.width, app.screen.height)
+const tilingSprite = new TilingSprite(texture, WORLDWIDTH, WORLDHEIGHT)
 
-app.stage.addChild(tilingSprite)
+// app.stage.addChild(tilingSprite)
+
+const grid = new PixiJSGrid(30).drawGrid()
 
 // app.ticker.add(() => {
 //   tilingSprite.tilePosition.x += 1
